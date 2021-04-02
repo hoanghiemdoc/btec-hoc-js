bc512f284c80fc3cc5006b99d340d4f7389b3fc4
nguyễn huy hoàng <73330590+hoanghiemdoc@users.noreply.github.com>
2021-04-02 09:21:53 +0700

    Add files via upload

---
+++ bt1.js
@@ -0,0 +1,183 @@
+// câu1 những tên biến không hợp lệ là:
+
+// let ᾩ = "something";
+// let ĦĔĽĻŎ = "hello";
+// let ĦĔĽĻŎ = "hello";
+
+
+//  câu2 các kiểu dữ liệu tên biến có các kiểu
+// kiểu Number;
+// kiểu String;
+// kiểu Boolean;
+// kiểu undefined;
+
+// var là biến có phạm vi rộng hơn 
+// const là một biến mà nó chỉ cố định một không thay đổi được
+// 4  sự khác nhau giữa let và const
+
+// let là một biến mà trong biến đấy có thể thay đổi dc
+// const là một biến mà ở trong không thể thay đổi dc
+
+
+// kiểu Boolean là kiểu dữ liệu true, fasle
+
+// boolean dẫn đến kết quả true fasle
+
+
+
+// câu 6
+// alert("you look beautifull today");
+
+// c7
+// let show = prompt("hi there, your name please?");
+// alert("hi phuong nam");
+
+// c8
+// let firtName = prompt("enter your first name");
+
+// let lastName = prompt(" enter your lastName");
+
+// alert("hi Nguyễn Phương Nam");
+
+
+// c9
+// let number = prompt("enter side length of the square");
+
+// alert("the square area is 100");
+
+// c10
+// let number2 = prompt("enter the radios of the cricle");
+// alert("the circle area is 314.0")
+
+// c11
+
+// let number3 = prompt("enter the temperature in celclus");
+// alert(10(c)=50.0(F));
+
+// c12
+
+
+// let a =  7;
+// for (let index = 0; index < a; index++) {
+//    console.log(index)
+    
+// }
+
+
+// c13
+// let a= 4;
+// for (let index = 0; index < 4; index++) {
+//     let b = prompt("enter n");
+//     console.log(index);
+    
+// }
+
+// c)
+
+// let a= 7;
+// for (let index = 3; index < 7; index++) {
+//     let b = prompt("enter n");
+//     console.log(index);
+// }
+
+
+// c 15
+
+// let n = prompt('Nhap gia tri cua N')
+// 		function giaithua(x)
+// 		{
+// 			let giaithua=1;
+// 			for(let i=1;i<=n;i++)
+// 			{
+// 				giaithua*=i
+// 			}
+// 			alert('Giai Thua bang '+giaithua)
+// 		}
+// var let=giaithua(n)
+
+// c16)
+
+// let show = prompt("how old are you");
+// let old = 12;
+// if(show < old){
+//     alert("you are not englaught to content");
+    
+// }
+// if (show > old) {
+//     alert("enjoy");
+// }
+
+
+// c17
+
+// let number = prompt("enter a number");
+// let number1 = 9;
+// if(number < 9) {
+//     alert("low haft of 9");
+// }
+
+// let number = prompt("enter a number");
+// let number1 = 7;
+// if(number > 7) {
+//     alert("hight haft of 9");
+// }
+
+
+// c18)
+
+// let program = prompt("n=");
+// let n= 14;
+
+// let program1 = prompt("x=");
+// let x= 5;
+
+// if(x = n) {
+//     alert(" 5 is in lower half of 14");
+// }
+
+
+// let program = prompt("n=");
+// let n= 14;
+
+// let program1 = prompt("x=");
+// let x= 10;
+
+// if(x = n) {
+//     alert(" 5 is in lower half of 14");
+// }
+
+// c17
+// let show = prompt("x=")
+// let a = 5;
+//     if(a % 2 != 0) {
+//       alert("5 is an old number");  
+// } 
+
+// let show = prompt("x=")
+// let a = 12;
+//     if(a % 2 == 0) {
+//       alert("12 is an even number");  
+// }  
+
+
+// c19
+// let body_Mass = prompt("your weight in kg");
+// let body =  5;
+// let max = 6;
+// let BMI =body  * max;
+// if (BMI < 16) {
+//     alert("")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
